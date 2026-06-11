ed8a928457d40635c2d9a8dfc5014cc4f2a2379c
VIPLAV KUMAR <viplavkumar3@gmail.com>
2026-06-11 22:03:43 +0530

    added hero image

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -7,8 +7,8 @@ import { fadeUp, stagger } from '../../utils/motionVariants'
 
 const iconMap = { Clock, Shield, Users, Heart }
 
-const HERO_BG = '/dwarka-pc-hero.png'
-const HERO_BG_MOBILE = '/dwarka-phone-hero.jpeg'
+const HERO_BG = '/dwarkadhish-main-hero.jpeg'
+const HERO_BG_MOBILE = '/dwarkadhish-main-hero.jpeg'
 
 export default function Hero() {
   const waMsg = 'Hello Shree Momai Cabs! I want to book a taxi. Please share details.'
